e6b0814b1ee3ef7775e8106bd0c0b73e4e44d477
keba0101 <keba0101@sina.com>
2023-02-16 11:30:22 +0800

    Site updated: 2023-02-16 11:30:12

--- js/nav.js
+++ js/nav.js
@@ -35,8 +35,3 @@ function scrollToTop(){
 //修复没有弄右键菜单的童鞋无法回顶部的问题
 document.getElementById("page-name").innerText = document.title.split(" | Ariasakaの小窝")[0];}
 
-
-作者: Ariasaka
-链接: https://yisous.xyz/posts/895003b5/#%E6%98%BE%E7%A4%BA%E6%A0%87%E9%A2%98
-来源: Ariasakaの小窝
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。

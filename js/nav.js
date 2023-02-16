b1f27b065da67aee107a781f036bd51cb7b0f16c
keba0101 <keba0101@sina.com>
2023-02-16 11:32:31 +0800

    Site updated: 2023-02-16 11:32:18

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,6 @@
-document.addEventListener('pjax:complete', tonav);
-document.addEventListener('DOMContentLoaded', tonav);
-//响应pjax
-function tonav(){
+//js有一个小问题：就是只要鼠标滚动不论哪里都会响应，即便你滚动的是子元素
+
+//2022.9.11 已修复，需要jq，请自行引入
 document.getElementById("name-container").setAttribute("style", "display:none");
 
 var position = $(window).scrollTop();
@@ -33,5 +32,8 @@ function scrollToTop(){
     btf.scrollToDest(0, 500);
 }
 //修复没有弄右键菜单的童鞋无法回顶部的问题
-document.getElementById("page-name").innerText = document.title.split(" | Ariasakaの小窝")[0];}
+document.getElementById("page-name").innerText = document.title.split(" | Ariasakaの小窝")[0];
+/*这里是去掉你的网站全局名称的设置，如果你不需要去掉，你可以写成：
+document.getElementById("page-name").innerText=document.title
 
+或者把你的网站的分隔符和全局网站名称加上去*/
--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ workbox.core.clientsClaim();
 
 // 注册成功后要立即缓存的资源列表
 // 具体缓存列表在gulpfile.js中配置，见下文
-workbox.precaching.precacheAndRoute([{"revision":"b05133d9cf52cbfc894de11aac8fcb94","url":"./index.html"},{"revision":"88aced9e00713346dbf5f92453a349b2","url":"./js/main.js"},{"revision":"e57d62bd4e417cf0c5e32301cab8310d","url":"./css/index.css"}],{
+workbox.precaching.precacheAndRoute([{"revision":"a43b4c5f93f4f0d51db6e030716c5c88","url":"./index.html"},{"revision":"88aced9e00713346dbf5f92453a349b2","url":"./js/main.js"},{"revision":"e57d62bd4e417cf0c5e32301cab8310d","url":"./css/index.css"}],{
     directoryIndex: null
 });
 

0cc77c8bee8ed336eee1a5652e61639fd3ba543f
keba0101 <keba0101@sina.com>
2023-11-03 20:52:38 +0800

    Site updated: 2023-11-03 20:52:35

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ workbox.core.clientsClaim();
 
 // 注册成功后要立即缓存的资源列表
 // 具体缓存列表在gulpfile.js中配置，见下文
-workbox.precaching.precacheAndRoute([{"revision":"6cbd9ba9346a95437406e5e5bcf8e7ec","url":"./index.html"},{"revision":"88aced9e00713346dbf5f92453a349b2","url":"./js/main.js"},{"revision":"b3ccbb45b785bfe5e81b2b06bc317930","url":"./css/index.css"}],{
+workbox.precaching.precacheAndRoute([{"revision":"c7c23075412d642951111a3c9454e272","url":"./index.html"},{"revision":"88aced9e00713346dbf5f92453a349b2","url":"./js/main.js"},{"revision":"b3ccbb45b785bfe5e81b2b06bc317930","url":"./css/index.css"}],{
     directoryIndex: null
 });
 
